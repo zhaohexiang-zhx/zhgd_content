9f6c2d1f88f01c01206048c2d152824cc1190782
zhaohexiang-zhx <zhx190523@163.com>
2020-08-27 14:52:52 +0800

    参数化数据：二级分类名称

--- debugtalk.py
+++ debugtalk.py
@@ -6,4 +6,11 @@ def sleep(n_secs):
 
 def get_timestamp():
     return str(int(time.time() * 10))
-    
+
+# 11位
+def get_timestamp2():
+    return str(int(time.time() * 10))
+
+# 5位
+def get_timestamp1():
+    return str(time.time()).replace(".", "")[6:10]
